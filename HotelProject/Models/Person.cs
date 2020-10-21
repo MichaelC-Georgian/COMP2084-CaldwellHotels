bad30a385dc6b55c12c090cb14b89e720b49a845
MichaelC-Georgian <71400323+MichaelC-Georgian@users.noreply.github.com>
2020-10-21 17:06:33 -0400

    Refactored code to work with authentication
    Refactored code to work with authentication (Original Project didn't have it selected)
    
    Had to start a new project, import various files and adjust where errors appeared

---
+++ HotelProject/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.Models
+{
+    public class Person
+    {
+        public int PersonID { get; set; } //PK
+        [Required]
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String PhoneNumber { get; set; }
+        public String EmailAddress { get; set; }
+        public String StreetAddress { get; set; }
+        public String City { get; set; }
+        public String Province { get; set; }
+        public String Country { get; set; }
+        public String PostalCode { get; set; }
+    }
+}
---
+++ HotelProject/Models/RoomStyle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.Models
+{
+    public class RoomStyle
+    {
+        public int StyleID { get; set; } //PK
+        [Required]
+        public String RoomType { get; set; }
+        public String RoomDescription { get; set; }
+    }
+}
